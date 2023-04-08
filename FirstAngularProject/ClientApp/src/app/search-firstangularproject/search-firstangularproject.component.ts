222a6a1b3111aa361a4633057c355ed563f3d9db
SteAlb89 <98278002+SteAlb89@users.noreply.github.com>
2023-04-08 13:41:57 +0200

    modifying the form

--- FirstAngularProject/ClientApp/src/app/search-firstangularproject/search-firstangularproject.component.ts
+++ FirstAngularProject/ClientApp/src/app/search-firstangularproject/search-firstangularproject.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Time } from '@angular/common';
 
 @Component({
   selector: 'app-search-firstangularproject',
@@ -7,16 +8,45 @@ import { Component } from '@angular/core';
 })
 export class SearchFirstangularprojectComponent {
 
-  searchResult: any = [
-  //  "American Airlines",
-  //"British Airways",
-  //"Lufthansa",
-  //"Bruxel Airlines",
-  //"Ryanair"
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: "Los Angeles" },
+      arrival: { time: Date.now().toString(), place: "Istanbul" },
+      price: "350",
+    },
+    {
+      airline: "Deutsche BA",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "Munchen" },
+      arrival: { time: Date.now().toString(), place: "Schiphol" },
+      price: "600",
+    },
+    {
+      airline: "British Airways",
+      remainingNumberOfSeats: 60,
+      departure: { time: Date.now().toString(), place: "London, England" },
+      arrival: { time: Date.now().toString(), place: "Vizzola-Ticino" },
+      price: "600",
+    },
   ]
   constructor() { }
 
   ngOnInit(): void {
 
   }
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
